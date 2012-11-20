71adea0cc2ca5d50c73ed7df290735de609e6874
root <dennisyu319@gmail.com>
2012-11-20 18:16:25 +0000

    added autoloaders in Sailthru_Client.php

--- sailthru/Sailthru_Client.php
+++ sailthru/Sailthru_Client.php
@@ -1,4 +1,11 @@
 <?php
+function _autoload($class_name) {
+    $dirname = dirname(dirname(__FILE__));
+    $path = "{$dirname}/sailthru/{$class_name}.php";
+    require $path;
+}
+
+spl_autoload_register("_autoload");
 /**
  *
  * Makes HTTP Request to Sailthru API server
