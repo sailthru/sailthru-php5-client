9ac0bd580ffecd1ddff002bad5d5846c9384df38
Wenjun Zhang <wzhang@sailthru.com>
2016-03-28 14:56:55 -0400

    add method to retrieve last rate limit info per action/method

--- tests/Sailthru_ClientTest.php
+++ tests/Sailthru_ClientTest.php
@@ -4,43 +4,39 @@ require(__DIR__ . '/../sailthru/Sailthru_Client_Exception.php');
 require(__DIR__ . '/../sailthru/Sailthru_Util.php');
 
 class Sailthru_ClientTest extends PHPUnit_Framework_TestCase {
-    public function setUp() {
-        $this->api_key = 'my_api_key';
-        $this->secret = 'my_secret';
-        $this->api_url = 'https://api.sailthru.com';
-        $this->sailthru_client = new Sailthru_Client($this->api_key, $this->secret, $this->api_url);
-    }
+    private $api_key = "my_api_key";
+    private $api_secret = 'my_secret';
+    private $api_url = 'https://api.sailthru.com';
 
     public function testDefaultTimeoutParameter() {
-        $this->sailthru_client = new Sailthru_Client($this->api_key, $this->secret, $this->api_url);
-        $this->assertTrue($this->sailthru_client->getTimeout() == 10000);
-        $this->assertTrue($this->sailthru_client->getConnectTimeout() == 10000);
+        $sailthru_client = new Sailthru_Client($this->api_key, $this->api_secret, $this->api_url);
+        $this->assertEquals(10000, $sailthru_client->getTimeout());
+        $this->assertEquals(10000, $sailthru_client->getConnectTimeout());
     }
 
     public function testCustomTimeoutParameter() {
-        $this->sailthru_client = new Sailthru_Client($this->api_key, $this->secret, $this->api_url,
+        $sailthru_client = new Sailthru_Client($this->api_key, $this->api_secret, $this->api_url,
                                                      array('timeout' => 1, 'connect_timeout' => 2));
-        $this->assertTrue($this->sailthru_client->getTimeout() == 1);
-        $this->assertTrue($this->sailthru_client->getConnectTimeout() == 2);
+        $this->assertEquals(1, $sailthru_client->getTimeout());
+        $this->assertEquals(2, $sailthru_client->getConnectTimeout());
     }
 
     public function testSendWhenTemplateNameIsInvalid() {
         $template_name = 'invalid_template';
-        $json_response = json_encode(array('error' => 14, 'errormsg' => 'Unknown template: ' . $template_name));
         $email = 'praj@sailthru.com';
-        $mock = $this->getMock('Sailthru_Client', array('send'), array($this->api_key, $this->secret, $this->api_url));
+        $json_response = json_encode(array('error' => 14, 'errormsg' => 'Unknown template: ' . $template_name));
+        $mock = $this->getMock('Sailthru_Client', array('send'), array($this->api_key, $this->api_secret, $this->api_url));
         $mock->expects($this->once())
                 ->method('send')
                 ->will($this->returnValue($json_response));
-         $this->assertEquals($json_response, $mock->send($template_name, 'praj@sailthru.com'));
+         $this->assertEquals($json_response, $mock->send($template_name, $email));
     }
-
 
     public function testSendWhenTemplateIsValid() {
         $template_name = 'my_template';
         $email = 'praj@sailthru.com';
         $json_response = json_encode(array('email' => $email, 'send_id' => 'some_unique_id', 'template' => $template_name, 'status' => 'unknown'));
-        $mock = $this->getMock('Sailthru_Client', array('send'), array($this->api_key, $this->secret, $this->api_url));
+        $mock = $this->getMock('Sailthru_Client', array('send'), array($this->api_key, $this->api_secret, $this->api_url));
         $mock->expects($this->once())
                 ->method('send')
                 ->will($this->returnvalue($json_response));
@@ -48,7 +44,7 @@ class Sailthru_ClientTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testApiPostWithValidJsonResponse() {
-        $mock = $this->getMock('Sailthru_Client', array('apiPost'), array($this->api_key, $this->secret, $this->api_url));
+        $mock = $this->getMock('Sailthru_Client', array('apiPost'), array($this->api_key, $this->api_secret, $this->api_url));
         $json_response = array(
             'email' => 'praj@infynyxx.com',
             'profile_id' => '4f284c28a3a627b6389bfb4c',
@@ -68,8 +64,7 @@ class Sailthru_ClientTest extends PHPUnit_Framework_TestCase {
      * @expectedException Sailthru_Client_Exception
      */
     public function testApiPostWithInvalidJsonResponse() {
-        $mock = $this->getMock('Sailthru_Client', array('apiPost'), array($this->api_key, $this->secret, $this->api_url));
-        $json_response = 'invalid JSON';
+        $mock = $this->getMock('Sailthru_Client', array('apiPost'), array($this->api_key, $this->api_secret, $this->api_url));
         $mock->expects($this->once())
             ->method('apiPost')
             ->will($this->throwException(new Sailthru_Client_Exception()));
@@ -78,6 +73,7 @@ class Sailthru_ClientTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testPrepareJsonPayload() {
+        $this->sailthru_client = new Sailthru_Client($this->api_key, $this->api_secret, $this->api_url);
         $method = new ReflectionMethod('Sailthru_Client', 'prepareJsonPayload');
         $method->setAccessible(true);
         $json_payload_without_binary_data = array(
@@ -93,6 +89,7 @@ class Sailthru_ClientTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testPrepareJsonPayloadWithBinaryData() {
+        $this->sailthru_client = new Sailthru_Client($this->api_key, $this->api_secret, $this->api_url);
         $method = new ReflectionMethod('Sailthru_Client', 'prepareJsonPayload');
         $method->setAccessible(true);
         $json_payload = array(
@@ -106,5 +103,25 @@ class Sailthru_ClientTest extends PHPUnit_Framework_TestCase {
         $invoked = $method->invoke($this->sailthru_client, $json_payload, $binary_data_param);
         $this->assertEquals($invoked['api_key'], $this->api_key);
         $this->assertEquals($invoked['file'], $binary_data_param['file']);
+    }
+
+    public function testParseRateLimitHeaders() {
+        $sailthru_client = new Sailthru_Client($this->api_key, $this->api_secret, $this->api_url);
+        $method = new ReflectionMethod("Sailthru_Client", "parseRateLimitHeaders");
+        $method->setAccessible(true);
+        $headers = <<<HEADERS
+HTTP/1.1 200 OK
+Date: Mon, 28 Mar 2016 14:43:05 GMT
+Server: Apache
+X-Rate-Limit-Limit: 40
+X-Rate-Limit-Remaining: 3
+X-Rate-Limit-Reset: 1459190520
+Content-Length: 2
+Content-Type: application/json"
+HEADERS;
+
+        $parsed_rate_limit = $method->invoke($sailthru_client, $headers);
+        $expected = ['limit' => 40, 'remaining' => 3, "reset" => 1459190520];
+        $this->assertEquals($expected, $parsed_rate_limit);
     }
 }
