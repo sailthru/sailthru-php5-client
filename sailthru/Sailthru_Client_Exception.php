aadf2a21718795b656e87c7df4c1eab9c7fd720d
Yavor Milchev <yavor@tradesy.com>
2016-08-01 15:06:52 -0700

    Add error codes

--- sailthru/Sailthru_Client_Exception.php
+++ sailthru/Sailthru_Client_Exception.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Handles Sailthru Client Exceptions
+ */
 class Sailthru_Client_Exception extends Exception {
-
+    /**
+     * Standardized exception codes.
+     */
+    const CODE_GENERAL = 1000;
+    const CODE_BAD_API_RESPONSE = 1001;
+    const CODE_INVALID_JSON = 1002;
 }
